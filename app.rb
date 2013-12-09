c625f6ef36352a9ed1aba45606128b07e42208f9
Amine TheDream <aminethedream@Amines-MacBook-Air.local>
2013-12-09 15:25:57 -0500

    add files

--- app.rb
+++ app.rb
@@ -1,10 +1,23 @@
 require 'sinatra/base'
 
+
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
-    get '/' do
-      "hello world!"
+    get '/hello-world' do
+    	@random_numbers = (1..20).to_a
+  
+    	erb :hello
     end
+
+    get '/artists' do
+    	erb :artists
+
+	  end
   end
 end
+
+# class Artist
+# 	def initialize
+# 	end
+# end
---
+++ lib/thing.rb
@@ -0,0 +1,9 @@
+class Thing
+	attr_accessor :name, :num
+
+	def initialize (name, num)
+		@name = name
+		@num = number
+	end
+	
+end
